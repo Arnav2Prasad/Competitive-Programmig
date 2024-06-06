510e75ce19fb4073980234fbb5452bcdf2bbd0d2
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2024-06-06 19:36:57 +0530

    Update sol.cpp

--- Dynamic Programmin/ques_55/sol.cpp
+++ Dynamic Programmin/ques_55/sol.cpp
@@ -13,3 +13,31 @@ public:
         return r[nums.size()-1]==1;
     }
 };
+
+
+
+------------------------------------------------
+    CHATGPT SOL
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;  // To track the maximum index we can reach
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (i > maxReach) {
+                // If the current index is greater than the maximum reachable index,
+                // we can't reach this point
+                return false;
+            }
+            // Update the maximum reachable index
+            maxReach = max(maxReach, i + nums[i]);
+            if (maxReach >= n - 1) {
+                // If we can reach or exceed the last index, return true
+                return true;
+            }
+        }
+        return maxReach >= n - 1;
+    }
+};
