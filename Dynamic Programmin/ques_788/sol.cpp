89a995103ad2600ce73a4c87ca523a2de4b70c97
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2024-06-15 15:36:43 +0530

    Create sol.cpp

---
+++ Dynamic Programmin/ques_788/sol.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            string str = to_string(i);
+
+            bool isValid = true;
+
+            bool isDifferent = false;
+
+            string rotatedStr = "";
+
+            for (char c : str) {
+
+                if (c == '0' || c == '1' || c == '8') {
+
+                    rotatedStr += c;
+
+                } else if (c == '2') {
+
+                    rotatedStr += '5';
+
+                    isDifferent = true;
+
+                } else if (c == '5') {
+
+                    rotatedStr += '2';
+
+                    isDifferent = true;
+
+                } else if (c == '6') {
+
+                    rotatedStr += '9';
+
+                    isDifferent = true;
+
+                } else if (c == '9') {
+
+                    rotatedStr += '6';
+
+                    isDifferent = true;
+
+                } else {
+
+                    isValid = false;
+
+                    break;
+
+                }
+
+            }
+
+            if (isValid && isDifferent && rotatedStr!= str) {
+
+                count++;
+
+            }
+
+        }
+
+        return count;
+    }
+};
